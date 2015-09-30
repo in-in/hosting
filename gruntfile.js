05e3011cc303276602ca9160edf7175d1959d6ca
in-in <timothybernerslee@gmail.com>
2015-09-30 22:10:18 +0300

    init commit

---
+++ gruntfile.js
@@ -0,0 +1,279 @@
+module.exports = function (grunt) {
+  'use strict';
+  require('time-grunt')(grunt);
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    sass: {
+      dev: {
+        options: {
+          includePaths: [
+            'node_modules/susy/sass',
+            'node_modules/breakpoint-sass/stylesheets',
+            'src/scss/',
+            'src/scss/base/',
+            'src/scss/layout/',
+            'src/scss/module/',
+            'src/scss/utils/'
+          ],
+          outputStyle: 'expanded',
+          sourceMap: true
+        },
+        files: {
+          "src/css/style.css": "src/scss/index.scss"
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer')(
+            {browsers: 'last 2 versions'}
+          )
+        ]
+      },
+      dist: {
+        src: 'src/css/style.css'
+      }
+    },
+
+    jade: {
+      dev: {
+        options: {
+          pretty: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/jade/',
+          src: ['*.jade'],
+          dest: 'src/',
+          ext: '.html'
+        }]
+      }
+    },
+
+    concat: {
+      dev: {
+        options: {
+          separator: ';\n'
+        },
+        src: [
+          'src/js/dev/*.js'
+        ],
+        dest: 'src/js/main.js'
+      }
+    },
+
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [
+            "src/*.html",
+            "src/css/*.css",
+            "src/js/*.js"
+          ]
+        },
+        options: {
+          watchTask: true,
+          port: 3008,
+          browser: 'chromium-browser',
+          server: {baseDir: "src"},
+          notify: false,
+          injectChanges: true,
+          ui: false
+        }
+      }
+    },
+
+//SVG
+
+    svgmin: {
+      options: {
+        plugins: [
+          {removeViewBox: false},
+          {removeUselessStrokeAndFill: true},
+          {removeMetadata: true}
+        ]
+      },
+      dev: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/img/svg/',
+            src: ['*.svg'],
+            dest: 'src/img/svgmin/',
+            ext: '.min.svg'
+          }
+        ]
+      }
+    },
+
+    svgstore: {
+      options: {
+        //cleanup: ['fill', 'stroke'],
+        inheritviewbox: true,
+        prefix: 'icon-',
+        svg: {
+          //xmlns: 'http://www.w3.org/2000/svg',
+          style: "display: none;"
+        }
+      },
+      dev: {
+        files: {
+          'src/jade/inc/svgsprite.jade': ['src/img/svgmin/*.min.svg']
+        }
+      }
+    },
+
+//CSSCOMB
+
+    csscomb: {
+      dev: {
+        options: {
+          config: '.csscomb.json'
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/scss',
+          src: ['**/*.scss',
+                '!**/_utils.scss',
+                '!**/_vars.scss',
+                '!**/_module.scss'
+               ],
+          dest: 'src/scss/',
+          ext: '.scss'
+        }]
+      }
+    },
+
+//WATCH
+
+    watch: {
+      sass: {
+        files: "src/scss/**/*.scss",
+        tasks: ['newer:csscomb:dev', 'sass:dev', 'postcss']
+      },
+      jade: {
+        files: "src/jade/**/*.jade",
+        tasks: ['newer:jade:dev']
+      },
+      js: {
+        files: 'src/js/dev/index.js',
+        tasks: ['concat']
+      },
+      svg: {
+        files: 'src/img/svg/*.svg',
+        tasks: ['newer:svgmin', 'svgstore']
+      }
+    },
+
+//BUILD TASKS
+
+    clean: {
+      build: {
+        src: ["build"]
+      },
+      svg: {
+        src: ["src/img/svgmin"]
+      }
+    },
+
+    copy: {
+      build: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: [
+              '**',
+              '!**/js/dev/**',
+              '!**/scss/**',
+              '!**/jade/**',
+              '!**/svg*/**'
+            ],
+            dest: 'build/'
+          }
+        ]
+      }
+    },
+
+    cssmin: {
+      build: {
+        files: {
+          'build/css/style.css': ['build/css/style.css']
+        }
+      }
+    },
+
+    htmlmin: {
+      build: {
+        options: {
+          removeComments: true,
+          removeCommentsFromCDATA: true,
+          removeEmptyAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true
+        },
+        files: {
+          'build/index.html': 'build/index.html'
+        }
+      }
+    },
+
+    uglify: {
+      build: {
+        files: {'build/js/main.js': 'build/js/main.js'}
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'src/img/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'build/img/'
+        }]
+      }
+    },
+
+    notify_hooks: {
+      options: {
+        enabled: true,
+        duration: 10
+      }
+    },
+
+//PUBLISH ON GITHUB PAGES
+
+    'gh-pages': {
+      options: {
+        base: 'build'
+      },
+      src: ['**']
+    }
+  });
+
+  
+  grunt.registerTask('svg', [
+    'clean:svg',
+    'svgmin',
+    'svgstore'
+  ]);
+  
+  grunt.registerTask('build', [
+    'clean:build',
+    'copy',
+    'cssmin',
+    'htmlmin',
+    'uglify',
+    'imagemin'
+  ]);
+  
+  grunt.registerTask('default', [
+    'browserSync',
+    'watch'
+  ]);
+};
