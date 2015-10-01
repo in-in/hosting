bc35cd272966512081ab8221ae8380644b797364
in-in <timothybernerslee@gmail.com>
2015-10-02 01:30:01 +0300

    update

--- build/js/analytics.js
+++ build/js/analytics.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'XX-XXXXXXXX-X', 'auto');
+ga('create', 'UA-66478234-2', 'auto');
 ga('send', 'pageview');
