ddd7ec38b3be57b55a59543198537f9b801b53e4
in-in <timothybernerslee@gmail.com>
2015-10-28 17:11:10 +0300

    Updates

---
+++ js/clients-slider.js
@@ -0,0 +1,7 @@
+var elem = document.querySelector('.service-clients--slider');
+var flkty = new Flickity(elem, {
+  cellAlign: 'center',
+  contain: true,
+  prevNextButtons: false,
+  wrapAround: true
+});
