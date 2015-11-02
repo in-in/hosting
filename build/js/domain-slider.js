a94b6a4a6c102d7001183b9bd0a7da0dde0e1a4e
in-in <timothybernerslee@gmail.com>
2015-11-02 21:28:00 +0300

    update

---
+++ build/js/domain-slider.js
@@ -0,0 +1,8 @@
+var elem = document.querySelector('.domain-slider');
+var flkty = new Flickity(elem, {
+  cellAlign: 'center',
+  contain: true,
+  prevNextButtons: false,
+  wrapAround: true,
+  initialIndex: 2
+});
